25fc1603b9adba16dc0bb72998625ef7ce83db6f
FarhanAli325706 <farhanlala706@gmail.com>
2019-11-11 13:36:37 +0500

    Sir, again please!

--- app/src/main/java/com/smsaz/fitnessenthusiast/FAQActivity.java
+++ app/src/main/java/com/smsaz/fitnessenthusiast/FAQActivity.java
@@ -1,14 +1,43 @@
 package com.smsaz.fitnessenthusiast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class FAQActivity extends AppCompatActivity {
-    // do this with recycler view
+
+    private LinkedList<String> questions;
+
+    private RecyclerView faqRecyclerView;
+    private RecyclerView.Adapter faqAdapter;
+    private RecyclerView.LayoutManager faqLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        questions = new LinkedList<String>();
+
+        for (int i = 0;i<5; i++){
+            questions.add(new String ("Q " + (i + 1) + ": Which is More Productive: Less Weight/More Reps or More Weight/Less Reps?" +
+                    "\n\nA: Both lifting methods offer their own distinct set of benefits. " +
+                    "Less weight and more reps leads to better endurance and faster lean muscle growth." +
+                    " More weight and less reps causes larger muscles and overall higher strength." +
+                    " A combination of both techniques may be the most productive strategy."));
+        }
+
+        faqRecyclerView = findViewById(R.id.faqRecyclerView);
+// Create an adapter and supply the data to be displayed.
+        faqAdapter = new FAQAdapter(this, questions);
+// Connect the adapter with the RecyclerView.
+        faqRecyclerView.setAdapter(faqAdapter);
+// Give the RecyclerView a default layout manager.
+        faqRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ app/src/main/java/com/smsaz/fitnessenthusiast/FAQAdapter.java
@@ -0,0 +1,55 @@
+package com.smsaz.fitnessenthusiast;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class FAQAdapter extends RecyclerView.Adapter<FAQAdapter.FAQViewHolder> {
+
+    LinkedList<String> questions;
+    private LayoutInflater layoutInflater;
+
+    public FAQAdapter(Context context, LinkedList<String> questions) {
+        this.questions = questions;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public FAQViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = layoutInflater.inflate(R.layout.faq_sample,parent,false);
+
+        return new FAQViewHolder(itemView,this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FAQViewHolder holder, int position) {
+        String currentQuestion = questions.get(position);
+        holder.question.setText(currentQuestion);
+    }
+
+    @Override
+    public int getItemCount() {
+        return questions.size();
+    }
+
+    public class FAQViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView question;
+        private FAQAdapter faqAdapter;
+
+        public FAQViewHolder(@NonNull View itemView, FAQAdapter faqAdapter) {
+            super(itemView);
+            question = itemView.findViewById(R.id.question);
+            this.faqAdapter = faqAdapter;
+        }
+    }
+}

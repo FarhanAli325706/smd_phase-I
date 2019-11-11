401bdcd608b4bfff0400d437ccbc6d56df800df7
FarhanAli325706 <farhanlala706@gmail.com>
2019-11-11 12:25:16 +0500

    please sir, pretty please

--- app/src/main/java/com/smsaz/fitnessenthusiast/LoginActivity.java
+++ app/src/main/java/com/smsaz/fitnessenthusiast/LoginActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,7 +20,19 @@ public class LoginActivity extends AppCompatActivity {
 
     public void toExercises(View view) {
         Intent intent=new Intent(this,ExercieList.class);
-        startActivity(intent);
+        EditText emailView=findViewById(R.id.edit_email);
+        EditText passwordView=findViewById(R.id.edit_password);
+        String email=emailView.getText().toString();
+        String password=passwordView.getText().toString();
+        if(email.matches("") || password.matches("")) {
+            Toast.makeText(this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+        }
+        else if(!(Patterns.EMAIL_ADDRESS.matcher(email).matches()))
+        {
+            Toast.makeText(this, "Enter a valid email.", Toast.LENGTH_SHORT).show();
+        }
+        else
+            startActivity(intent);
     }
 
     public void toSignup(View view) {
--- app/src/main/java/com/smsaz/fitnessenthusiast/SignupActivity.java
+++ app/src/main/java/com/smsaz/fitnessenthusiast/SignupActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -15,8 +18,28 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void toLogin(View view) {
-        Intent intent=new Intent(this,LoginActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        EditText usernameView = findViewById(R.id.usernameSignup);
+        EditText emailView = findViewById(R.id.emailSignup);
+        EditText passwordView = findViewById(R.id.passwordSignup);
+        EditText confirmPasswordView = findViewById(R.id.confirmPasswordSignup);
+
+        String username = usernameView.getText().toString();
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+        String confirmPassword = confirmPasswordView.getText().toString();
+
+        if (username.matches("") || email.matches("") ||
+                password.matches("") || confirmPassword.matches("")) {
+            Toast.makeText(this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Enter a valid Email", Toast.LENGTH_SHORT).show();
+        } else if (!password.matches(confirmPassword)){
+            Toast.makeText(this, "Password Mismatch", Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(intent);
+        }
     }
 
 }

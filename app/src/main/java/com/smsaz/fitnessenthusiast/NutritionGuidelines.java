838ec35555dcedacf3dc157b76284c337e7d584b
Syed Muhammad Sajjad <sajjadalizaidi00@gmail.com>
2019-12-01 00:35:38 +0500

    changed color scheme of nutrition guidelines and FAQs screen and implemented back button in these screens

--- app/src/main/java/com/smsaz/fitnessenthusiast/NutritionGuidelines.java
+++ app/src/main/java/com/smsaz/fitnessenthusiast/NutritionGuidelines.java
@@ -1,14 +1,31 @@
 package com.smsaz.fitnessenthusiast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NutritionGuidelines extends AppCompatActivity {
+
+    //Added for Back button implementation
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition_guidelines);
+
+        //Added for Back button implementation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // In `OnCreate();`
     }
 }
--- app/src/main/java/com/smsaz/fitnessenthusiast/faq/FAQActivity.java
+++ app/src/main/java/com/smsaz/fitnessenthusiast/faq/FAQActivity.java
@@ -1,14 +1,18 @@
 package com.smsaz.fitnessenthusiast.faq;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.smsaz.fitnessenthusiast.R;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class FAQActivity extends AppCompatActivity {
 
@@ -18,10 +22,25 @@ public class FAQActivity extends AppCompatActivity {
     private RecyclerView.Adapter faqAdapter;
     private RecyclerView.LayoutManager faqLayoutManager;
 
+    //Added for Back button implementation
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        //Added for Back button implementation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // In `OnCreate();`
 
         questions = new LinkedList<String>();
 
